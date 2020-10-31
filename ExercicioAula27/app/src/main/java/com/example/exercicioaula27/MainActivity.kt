2bc772d7151e2071932c75e5949c17b8e4341cbc
Roberto Freixeira <falconeri@ymail.com>
2020-10-30 22:26:39 -0300

    Aula 27 [2]

--- ExercicioAula27/app/src/main/java/com/example/exercicioaula27/MainActivity.kt
+++ ExercicioAula27/app/src/main/java/com/example/exercicioaula27/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.exercicioaula27
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val users = mutableListOf<User>()
+
+        users.add(User("Alberto","Lembra de mim?", "SEMANA PASSADA",12))
+        users.add(User("Bernardo","Tchau, boa noite", "ONTEM",0))
+
+
+
+
     }
 }
